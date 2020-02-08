7bc625aa64c89caa9fbff2d2a4eddfd05490b6c2
Josh <jspri17@gmail.com>
2020-02-08 21:12:34 +0000

    .

--- src/landing_controller.cpp
+++ src/landing_controller.cpp
@@ -2,7 +2,18 @@
 
 int main(int argc, char** argv)
 {
+	ros::init(argc, argv, "landing_controller");
+	ros::NodeHandle node_handle;
 	ROS_INFO("Hello, ROS World!");
+
+	ros::Rate loop_rate(1);
+	while( ros::ok() )
+	{
+//		ROS_INFO("Spinning...");
+
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 
 	return 0;
 }
