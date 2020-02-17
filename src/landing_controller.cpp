5afde9d227411dc751dacfb3a9372e6fdc7db1c7
Josh <jspri17@gmail.com>
2020-02-17 18:17:15 +0000

    Maybe found the correct way to mulitply the quaternions to put the relative_pose into the drone's coordinate system.

--- src/landing_controller.cpp
+++ src/landing_controller.cpp
@@ -16,6 +16,7 @@
 
 // for Gazebo simulation
 #include <gazebo_msgs/LinkStates.h>
+#include <gazebo_msgs/LinkState.h>
 
 using namespace std;
 
@@ -40,16 +41,21 @@ ros::Duration abort_time(0.5);
 tf2::Transform iris_base_link_transform;
 tf2::Transform gimbal_base_link_transform;
 tf2::Transform gimbal_tilt_link_transform;
-const std::string iris_base_link_name   = "iris_demo::iris_demo::iris::base_link";
-const std::string gimbal_base_link_name = "iris_demo::iris_demo::gimbal_small_2d::base_link";
-const std::string gimbal_tilt_link_name = "iris_demo::iris_demo::gimbal_small_2d::tilt_link";
+const std::string ground_plane_link_name = "ground_plane::link";
+const std::string iris_base_link_name    = "iris_demo::iris_demo::iris::base_link";
+const std::string gimbal_base_link_name  = "iris_demo::iris_demo::gimbal_small_2d::base_link";
+const std::string gimbal_tilt_link_name  = "iris_demo::iris_demo::gimbal_small_2d::tilt_link";
 
+tf2::Transform ground_plane_link_transform;
 tf2::Quaternion imu_orientation;
 tf2::Transform imu_transform;
 tf2::Quaternion camera_imu_orientation;
 tf2::Transform camera_imu_transform;
 
 geometry_msgs::Pose iris_pose;
+
+gazebo_msgs::LinkState link_state_message;
+ros::Publisher landing_pad_estimate_publisher;
 
 void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
 {
@@ -93,7 +99,11 @@ void link_states_callback(const gazebo_msgs::LinkStates::ConstPtr& msg)
 {
 	for(int i = 0; i < msg->name.size(); i ++)
 	{
-		if( msg->name[i] == iris_base_link_name )
+		if( msg->name[i] == ground_plane_link_name )
+		{
+			tf2::fromMsg(msg->pose[i], ground_plane_link_transform);
+		}
+		else if( msg->name[i] == iris_base_link_name )
 		{
 			tf2::fromMsg(msg->pose[i], iris_base_link_transform);
 
@@ -164,8 +174,12 @@ int main(int argc, char** argv)
 	landing_target_raw_publisher = node_handle.advertise<mavros_msgs::LandingTarget>("/mavros/landing_target/raw", 1000);
 	
 	landing_pad_relative_pose_publisher = node_handle.advertise<geometry_msgs::Pose>("/landing_pad/relative_pose", 1000);
-	landing_pad_position_publisher = node_handle.advertise<geometry_msgs::Vector3>("/landing_pad/global_position", 1000);
-	
+
+	// /landing_pad/global_position is for the landing_pad_estimate_plugin
+//	landing_pad_position_publisher = node_handle.advertise<geometry_msgs::Vector3>("/landing_pad/global_position", 1000);
+	landing_pad_position_publisher = node_handle.advertise<geometry_msgs::Vector3>("/landing_pad/global_pose", 1000);
+//	landing_pad_estimate_publisher = node_handle.advertise<gazebo_msgs::LinkState>("/gazebo/set_link_state", 1000);
+
 	ros::Rate loop_rate(20);
 	while( ros::ok() )
 	{
@@ -173,14 +187,56 @@ int main(int argc, char** argv)
 		ros::spinOnce();
 	
 		tf2::Quaternion NED_quaternion;
-		NED_quaternion.setRPY(0, 3.1415926/2, 0);
+		NED_quaternion.setRPY(0, -3.1415926/2, 0);
 		tf2::Transform NED;
 		NED.setRotation(NED_quaternion);
 
+		double imu_pitch, imu_roll, imu_yaw;
+		double cam_pitch, cam_roll, cam_yaw;
+		tf2::Matrix3x3(imu_orientation).getEulerYPR(imu_yaw, imu_pitch, imu_roll);
+		tf2::Matrix3x3(camera_imu_orientation).getEulerYPR(cam_yaw, cam_pitch, cam_roll);
+
+		ROS_INFO("iris imu YPR: <%0.3f, %0.3f, %0.3f>", imu_yaw, imu_pitch, imu_roll);
+		ROS_INFO(" cam imu YPR: <%0.3f, %0.3f, %0.3f>", cam_yaw, cam_pitch, cam_roll);
+
+//		tf2::Quaternion target_orientation = camera_imu_orientation;
+
+/*
+		tf2::Quaternion target_orientation;
+		target_orientation.setEuler(imu_yaw, 0, 0);
+		//target_orientation.setEuler(0, 0, 0);
+		target_orientation = target_orientation.normalize();
+		target_orientation *= camera_imu_orientation;
+*/
+
+/*
+		tf2::Quaternion yaw;
+		yaw.setEuler(0, imu_yaw, 0);
+		yaw = yaw.normalize();
+*/
+//		tf2::Quaternion target_orientation = camera_imu_orientation * imu_orientation;
+
+		// I think this one works.
+		tf2::Quaternion target_orientation = imu_orientation.inverse() * camera_imu_orientation;
+
+//		tf2::Quaternion target_orientation = camera_imu_orientation * (imu_orientation * camera_imu_orientation.inverse());
+		
+		double target_yaw, target_pitch, target_roll;
+		tf2::Matrix3x3(target_orientation).getEulerYPR(target_yaw, target_pitch, target_roll);
+		ROS_INFO("target_orientation YPR: <%0.3f, %0.3f, %0.3f>", target_yaw, target_pitch, target_roll);
+
+//		tf2::Quaternion target_orientation = camera_imu_orientation * imu_orientation;
+
+		// camera then yaw seems not to work at all
+//		tf2::Quaternion target_orientation = camera_imu_orientation * yaw;
+//		tf2::Quaternion target_orientation = yaw * camera_imu_orientation;
+
 		// create transform from camera to drone coordinate frame
-//		tf2::Transform gimbal_to_drone = camera_imu_transform * imu_transform;
+//		tf2::Transform gimbal_to_drone = camera_imu_transform;
+		tf2::Transform gimbal_to_drone;
+		gimbal_to_drone.setRotation( target_orientation );
 //		tf2::Transform gimbal_to_drone = camera_imu_transform.inverse();// * NED;
-		tf2::Transform gimbal_to_drone = camera_imu_transform;
+//		tf2::Transform gimbal_to_drone = camera_imu_transform * imu_transform * ground_plane_link_transform;
 
 		// necessary overhead for tf2::doTransform
 		geometry_msgs::TransformStamped gimbal_to_drone_stamped;
@@ -198,7 +254,20 @@ int main(int argc, char** argv)
 			position.x = iris_pose.position.x + landing_pad_relative_pose.position.x;
 			position.y = iris_pose.position.y + landing_pad_relative_pose.position.y;
 			position.z = max(iris_pose.position.z + landing_pad_relative_pose.position.z, 0.1);
+			//position.x = landing_pad_relative_pose.position.x;
+			//position.y = landing_pad_relative_pose.position.y;
+			//position.z = max(iris_pose.position.z + landing_pad_relative_pose.position.z, 0.1);
+
+			// for visual update
 			landing_pad_position_publisher.publish(position);
+
+//			link_state_message.link_name = "ground_plane::landing_pad_estimate_link";
+//			link_state_message.pose.position = landing_pad_relative_pose.position;
+//			link_state_message.reference_frame = iris_base_link_name;
+//			link_state_message.pose.position.x = position.x;
+//			link_state_message.pose.position.y = position.y;
+//			link_state_message.pose.position.z = position.z;
+//			landing_pad_estimate_publisher.publish(link_state_message);
 
 			send_landing_target_message();
 		}
