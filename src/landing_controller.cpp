a8c3446d06c1f93653931415b240831b3e1916c8
Josh <jspri17@gmail.com>
2020-03-30 13:58:43 +0000

    Changed code structure to have .h and .cpp files.

--- src/landing_controller.cpp
+++ src/landing_controller.cpp
@@ -1,68 +1,6 @@
-#include <ros/ros.h>
-#include <mavros_msgs/PositionTarget.h>
-#include <std_msgs/Bool.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Float64.h>
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <geometry_msgs/Vector3.h>
-#include <visualization_msgs/MarkerArray.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2/transform_datatypes.h>
-#include <sensor_msgs/Imu.h>
-#include <tf2/LinearMath/Transform.h>
-#include <cmath>
-#include <nav_msgs/Odometry.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/transform_listener.h>
+#include <landing_controller.h>
 
 using namespace std;
-
-ros::Publisher landing_pad_camera_pose_publisher;
-ros::Publisher landing_pad_relative_pose_stamped_publisher;
-ros::Publisher landing_pad_global_pose_stamped_publisher;
-ros::Publisher setpoint_raw_local_publisher;
-ros::Publisher displacement_n_publisher;
-ros::Publisher displacement_e_publisher;
-ros::Publisher displacement_u_publisher;
-ros::Publisher displacement_n_setpoint_publisher;
-ros::Publisher displacement_e_setpoint_publisher;
-ros::Publisher displacement_u_setpoint_publisher;
-ros::Publisher pid_enable_n_publisher;
-ros::Publisher pid_enable_e_publisher;
-ros::Publisher pid_enable_u_publisher;
-
-std_msgs::Float64 std_msgs_zero;
-std_msgs::Float64 setpoint_n;
-std_msgs::Float64 setpoint_e;
-std_msgs::Float64 setpoint_u;
-std_msgs::Bool std_msgs_true;
-std_msgs::Bool std_msgs_false;
-
-geometry_msgs::PoseStamped landing_pad_camera_pose;
-geometry_msgs::PoseStamped landing_pad_whycon_pose;
-geometry_msgs::PoseStamped landing_pad_apriltag_pose;
-geometry_msgs::PoseStamped landing_pad_relative_pose_stamped;
-geometry_msgs::PoseStamped landing_pad_relative_pose_stamped_straightened;
-geometry_msgs::PoseStamped landing_pad_relative_pose_absolute_yaw_stamped;
-geometry_msgs::PoseStamped landing_pad_global_pose_stamped;
-geometry_msgs::PoseStamped local_position_pose_stamped;
-geometry_msgs::Vector3 target_velocity;
-
-tf2_ros::Buffer transform_buffer;
-
-bool stopped = false;
-
-// timing
-ros::Time last_detection_time(0);
-ros::Duration abort_time(0.5);
-ros::Time last_target_send_time(0);
-ros::Duration target_send_interval(0.5);
-
-ros::Publisher landing_pad_estimate_publisher;
 
 // remove rotation from a pose by rotating it by the inverse of its rotation
 geometry_msgs::PoseStamped straighten_pose( const geometry_msgs::PoseStamped & _pose_in )
@@ -422,7 +360,7 @@ int main(int argc, char** argv)
 
 				}
 				// approach using velocity
-//				set_velocity_target_neu( target_velocity );
+				set_velocity_target_neu( target_velocity );
 			}
 		}
 
